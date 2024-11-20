50ea5c1a7afc84dba4792646a4d8c7088c3ab87b
pratap <udaypratapsinghsisondiya@gmail.com>
2024-11-20 20:14:26 +0530

    added menifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'React Vite PWA',
+        short_name: 'ReactPWA',
+        description: 'A React app with PWA capabilities using Vite',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
