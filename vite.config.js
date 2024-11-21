15c2b2e14fbb013d6a93f1ce0a1a5dda11c70380
pratap <udaypratapsinghsisondiya@gmail.com>
2024-11-21 19:00:09 +0530

    kk

--- vite.config.js
+++ vite.config.js
@@ -20,12 +20,14 @@ export default defineConfig({
           {
             src: '/192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       },
